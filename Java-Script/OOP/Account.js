35438111e43fd64b4cd36d08789239e803187fda
anshul4141 <anshulprajapati691@gmail.com>
2025-12-03 16:48:05 +0530

    done

---
+++ Java-Script/OOP/Account.js
@@ -0,0 +1,36 @@
+class Account {
+
+
+    constructor(accountNumber, accountType, balance) {
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = balance;
+    }
+
+    // setAccountNumber(accountNumber) {
+    //     this.accountNumber = accountNumber;
+    // }
+
+    getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    // setAccountType(accountType) {
+    //     this.accountType = accountType;
+    // }
+
+    getAccountType() {
+        return this.accountType;
+    }
+
+    // setBalance(balance) {
+    //     this.balance = balance;
+    // }
+
+    getBalance() {
+        return this.balance;
+    }
+
+    // deposit function
+    // withdraw function
+}
